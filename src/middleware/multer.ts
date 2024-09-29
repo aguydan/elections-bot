e8061d48cba0b0b38c815c5f79ee1220858fc2a8
Daniel Pivovarov <danielpivovarov1@gmail.com>
2024-09-29 19:57:53 +0300

    fix multer config typo

--- src/middleware/multer.ts
+++ src/middleware/multer.ts
@@ -7,8 +7,8 @@ const Config = require('../../config/config.json');
 
 const storage = multer.diskStorage({
     destination(req, file, cb) {
-        mkdirp.sync(`${Config.api.uploadsDir}/${Config.api.uploadsDir}`);
-        cb(null, `${Config.api.uploadsDir}/${Config.api.uploadsDir}`);
+        mkdirp.sync(`${Config.api.staticDir}/${Config.api.uploadsDir}`);
+        cb(null, `${Config.api.staticDir}/${Config.api.uploadsDir}`);
     },
     filename(req, file, cb) {
         const uniqueSuffix = Date.now() + Math.round(Math.random() * 1e9);
